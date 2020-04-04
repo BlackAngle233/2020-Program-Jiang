1afb66c0f2806f776c040814e93484adcec055ee
Zhuoheng Shen <1352293442@qq.com>
2020-04-04 20:02:10 +0800

    Update

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -1,20 +1,36 @@
 ﻿#include <iostream>
+using namespace std;
 
-int getRevseOrders(int num[], int len)
-{
-
+int getRevseOrders(int num[], int len) {
+	int revseOrders = 0;
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = i + 1; j < len; j++) {
+			if (i > j) {
+				revseOrders++;
+			}
+		}
+	}
+	return revseOrders;
 }
 
-void printPairs(int num[], int len)
-{
 
+void printPairs(int num[],int len)
+{
+	
+	for (int i = 0; i < len-1; i++) {
+		for (int j = i+1; j < len; j++) {
+			cout << "(" << num[i] << "," <<num[j] <<")"<< endl;
+		}
+	}
 }
 
 void test()
 {
 	int num1[] = { 1, 3 , 4 , 7,  2 };
 	int num2[] = { 9, 8, 7, 6, 5 };
-
+	//printPairs(num1, 5);
+	int n = getRevseOrders(num1, 5);
+	cout <<n <<endl;
 	// task  
 	// 1. print out all pairs 
 	// 2 . compute reverse orders 
@@ -22,5 +38,5 @@ void test()
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	test();
 }
