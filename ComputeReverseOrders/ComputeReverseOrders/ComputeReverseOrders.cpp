6514ebd0a9ee2d0cf9953a15bec3492a8e27fbd5
hiddenpiggy <feifei121290@sina.com>
2020-04-05 02:03:23 +0800

    张洪琪作业

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -1,26 +1,27 @@
-﻿#include <iostream>
+#include <cstdio>
 
-int getRevseOrders(int num[], int len)
+
+using namespace std;
+
+int getReverseOrders(int a[], int len)
 {
+    int ans = 0;
+    for(int i = 0; i < len-1; i++)
+    {
+        for(int j = i+1; j < len; j++)
+        {
+            if(a[i] > a[j]) ans++;
+            printf("%d %d\n",a[i],a[j]);
+        } 
+    }
 
+    return ans;
 }
 
-void printPairs(int num[], int len)
-{
-
-}
-
-void test()
-{
-	int num1[] = { 1, 3 , 4 , 7,  2 };
-	int num2[] = { 9, 8, 7, 6, 5 };
-
-	// task  
-	// 1. print out all pairs 
-	// 2 . compute reverse orders 
-}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a[4]{4,3,2,1};
+    printf("%s %d", "The number of ReverseOrder numbers of given array is:",getReverseOrders(a,4));
+    return 0;
 }
