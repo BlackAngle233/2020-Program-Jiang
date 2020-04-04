77ce7be8aa9249b40c3aad865b3a5a541d39cd04
amidu <63142813+amidu6@users.noreply.github.com>
2020-04-04 17:36:43 +0800

    作业

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -2,25 +2,70 @@
 
 int getRevseOrders(int num[], int len)
 {
-
+	int count = 0;
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 0; j < len; j++)
+		{
+			if (i < j && num[i] > num[j])
+			{
+				count++;
+			}
+		}
+	}
+	return count;
 }
 
 void printPairs(int num[], int len)
 {
-
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 0; j < len; j++)
+		{
+			if (i != j)
+			{
+				cout << "(" << num[i] << "," << num[j] << ")" << " ";
+			}
+		}
+	}
 }
 
 void test()
 {
 	int num1[] = { 1, 3 , 4 , 7,  2 };
 	int num2[] = { 9, 8, 7, 6, 5 };
-
-	// task  
-	// 1. print out all pairs 
-	// 2 . compute reverse orders 
+	printPairs(num1, 5);
+	cout << endl << "num1逆序数是：" << getRevseOrders(num1, 5) << endl;
+	printPairs(num2, 5);
+	cout << endl << "num2逆序数是：" << getRevseOrders(num2, 5) << endl;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	/*int a[65536];
+	int len;
+	int count = 0;
+	cout << "请输入数字的总数:";
+	cin >> len;
+	for (int i = 0; i < len; i++)
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 0; j < len; j++)
+		{
+			if (i < j && a[i] > a[j])
+			{
+				count++;
+			}
+			if (i != j)
+			{
+				cout << "(" << a[i] << "," << a[j] << ")" << " ";
+			}
+		}
+	}
+	cout << endl << "逆序数是：" << count << endl;*/
+	test();
+	return 0;
 }
