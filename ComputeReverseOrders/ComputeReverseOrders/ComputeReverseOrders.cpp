7e333d2f5bce698a4a2122ce54ebb5ca9c1cd65c
alan918 <1258164439@qq.com>
2020-04-05 16:23:39 +0800

    test1Update

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -1,44 +1,51 @@
-﻿#include <iostream>
+﻿#include"pch.h"
+#include <iostream>
 
 using namespace std;
 
-int getRevseOrders(int num1[], int len)
+int n1 = 0;
+int n2 = 0;
+int num1[] = { 1, 3 , 4 , 7,  2 };//求逆序对
+int num2[] = { 9, 8, 7, 6, 5 };// 求全部排列
+int getrevseorders(int num2[], int len)
 {
 	int i = 0;
 	int j = 0;
-	int len = 5;
-	for (j = 1; j <= len; j++) {
-		for (i = 1; i <= len; i++) {
-			if (num1[j] < num1[i]) {
-				n1 += 1；
-					cout << num1[j]num1[i] << endl;
+	for (j = 0; j <= len; j++) {
+		for (i = 0; i <= len; i++) {
+			if (num1[j] > num1[i] && i > j) {
+				n1 += 1;
+				cout << num1[j] << num1[i] << endl;
 			}
-				
+
 		}
 	}
 	return n1;
 }
 
-void printPairs(int num[], int len)
+int printpairs(int num2[], int len)
 {
-
-}
-
-void test()
-{
-	int num1[] = { 1, 3 , 4 , 7,  2 };
-	int num2[] = { 9, 8, 7, 6, 5 };
-	int n1 = 0;
-	int n2 = 0;
-	getRevseOrders(int num[], int len)
-    printPairs(int num[], int len)
-	cout << "reverse orders:"<<n1<<"     all pairs:" <<n2<< endl;
-	// task  
-	// 1. print out all pairs 
-	// 2 . compute reverse orders 
+	int i = 0;
+	int j = 0;
+	for (j = 0; j <= len; j++)
+	{
+		for (i = 0; i <= len; i++)
+		{
+			if (i != j)
+			{
+				n2 += 1;
+				cout << num2[j] << num2[i] << endl;
+			}
+		}
+	}
+	return n2;
 }
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	cout << "reverse orders :" << endl;
+	getrevseorders(num1, 4);
+	cout << "all pairs:" << endl;
+	printpairs(num2, 4);
+	cout << "reverse orders number:" << n1 << "     all pairs:" << n2 << endl;
 }
