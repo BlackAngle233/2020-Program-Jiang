ad1c38829e4f549d94159b42a0d65f0dc3338b55
Medemus <1937294341@qq.com>
2020-04-06 17:00:47 +0800

    update hw01

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -2,12 +2,31 @@
 
 int getRevseOrders(int num[], int len)
 {
+	int res{ 0 };
+	if (len < 2) {
+		return 0;
+	}
 
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = i + 1; j < len; j++) {
+			if (num[i] > num[j]) {
+				res++;
+			}
+		}
+	}
+	return res;
 }
 
 void printPairs(int num[], int len)
 {
-
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = i + 1; j < len; j++) {
+			if (num[i] > num[j]) {
+				std::cout << num[i] << "    " << num[j] << std::endl;
+			}
+		}
+	}
+	//std::cout << "------------------------" << std::endl;
 }
 
 void test()
@@ -18,9 +37,19 @@ void test()
 	// task  
 	// 1. print out all pairs 
 	// 2 . compute reverse orders 
+	int r1 = getRevseOrders(num1, 5);
+	std::cout << r1 << std::endl;
+	printPairs(num1, 5);
+
+
+
+	int r2 = getRevseOrders(num2, 5);
+	std::cout << r2 << std::endl;
+	printPairs(num2, 5);
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	test();
+	system("pause");
 }
