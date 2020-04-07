0d5beea26d902f3acef4f734c163aa954ae3eaa4
fragilefiled <58097731+fragilefiled@users.noreply.github.com>
2020-04-07 15:14:00 +0800

    Revert "OK"
    This reverts commit cc15dd119491d763ea650f093e6b04ae2d69ae3a.

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -1,35 +1,20 @@
-﻿#include<iostream>
-using namespace std;
+﻿#include <iostream>
+
 int getRevseOrders(int num[], int len)
 {
-	int count = 0;
-	for (int a = 0; a < len - 1; a++) {
-		for (int b = a + 1; b < len; b++) {
-		
-			if (num[b] < num[a])
-				count++;
-		}
-	}
-	return count;
+
 }
 
 void printPairs(int num[], int len)
-{   
-	for (int a = 0;a<len-1; a++) {
-		for(int b=a+1;b<len;b++)
-		cout << num[a]<<","<<num[b] << endl;
-	}
-            
+{
+	int i = 0;
 }
 
 void test()
 {
 	int num1[] = { 1, 3 , 4 , 7,  2 };
 	int num2[] = { 9, 8, 7, 6, 5 };
-	printPairs(num1,5);
-	cout << "the num of reverse pairs :"<<getRevseOrders(num1, 5) << endl;
-	printPairs(num2, 5);
-	cout << "the num of reverse pairs :" << getRevseOrders(num2, 5) << endl;
+
 	// task  
 	// 1. print out all pairs 
 	// 2 . compute reverse orders 
@@ -37,5 +22,5 @@ void test()
 
 int main()
 {
-	test();
+    std::cout << "Hello World!\n";
 }
