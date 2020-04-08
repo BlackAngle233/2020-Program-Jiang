b05a4c6bb113f5b5e9fd6faec17f1cd91b81b11c
giraffe1217 <51290300+giraffe1217@users.noreply.github.com>
2020-04-08 21:29:55 +0800

    update

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -2,12 +2,26 @@
 
 int getRevseOrders(int num[], int len)
 {
-
+	int count = 0;
+	for (int i = 0;i < len;i++) {
+		for (int j = i + 1;j < len;j++) {
+			if (num[i] > num[j]) {
+				count++;
+			}
+		}
+	}
+	return count;
 }
 
 void printPairs(int num[], int len)
 {
-
+	for (int i = 0;i < len;i++) {
+		for (int j = i + 1;j < len;j++) {
+			if (num[i] > num[j]) {
+				std::cout << "(" << num[i] << "," << num[j] << ")" << std::endl;
+			}
+		}
+	}
 }
 
 void test()
@@ -15,12 +29,17 @@ void test()
 	int num1[] = { 1, 3 , 4 , 7,  2 };
 	int num2[] = { 9, 8, 7, 6, 5 };
 
-	// task  
-	// 1. print out all pairs 
-	// 2 . compute reverse orders 
+	std::cout << "num1逆序数个数为" << getRevseOrders(num1, 5) << "个" << std::endl;
+	std::cout << "num1逆序对有: " << std::endl;
+	printPairs(num1, 5);
+
+	std::cout << "num2逆序数个数为" << getRevseOrders(num2, 5) << "个" << std::endl;
+	std::cout << "num2逆序对有: " << std::endl;
+	printPairs(num2, 5);
+
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	test();
 }
