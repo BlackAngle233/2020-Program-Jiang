a02b9402237aadb7f56c9638dbff53756a044525
JiCheng1 <1093501020@qq.com>
2020-04-10 17:08:09 +0800

    update

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -1,20 +1,38 @@
 ﻿#include <iostream>
+using namespace std;
 
 int getRevseOrders(int num[], int len)
 {
-
+	int i = 0,count = 0;
+	while (i <= len - 1)
+	{
+		for (int j = i+1; j <= len - 1; j = j + 1)
+		{
+			if (num[i] > num[j])
+			{
+				count = count + 1;
+			}
+		}
+		i = i + 1;
+	}
+	return count;
 }
 
 void printPairs(int num[], int len)
 {
-
+	int i = 0;
+	while (i <= len - 1)
+	{
+		for (int j=i+1; j <= len - 1; j = j + 1)
+		{
+				cout << '(' << num[i] << ',' << num[j] << ')' << endl;
+		}
+		i = i + 1;
+	}
 }
 
 void test()
 {
-	int num1[] = { 1, 3 , 4 , 7,  2 };
-	int num2[] = { 9, 8, 7, 6, 5 };
-
 	// task  
 	// 1. print out all pairs 
 	// 2 . compute reverse orders 
@@ -22,5 +40,11 @@ void test()
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+	int num1[] = { 1, 3, 4 ,7, 2 };
+	int num2[] = { 9, 8, 7, 6, 5 };
+	printPairs(num1, 5);
+	cout <<"逆序数为："<<getRevseOrders(num1, 5) << endl;
+	printPairs(num2, 5);
+	cout <<"逆序数为："<<getRevseOrders(num2, 5) << endl;
 }
