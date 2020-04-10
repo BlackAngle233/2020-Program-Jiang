e1e3c49a6c2cfd44d14cb8080eb8134c1303830d
Lulili33 <763333+Lulili33@users.noreply.github.com>
2020-04-10 22:40:15 +0800

    update

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -1,19 +1,41 @@
 ﻿#include <iostream>
+using namespace std;
 
 int getRevseOrders(int num[], int len)
 {
-
+	int n = 0;
+	cout << "此数组的逆序数为：";
+	for (int i = 0; i < len; i++)
+	{
+		for (int a = i; a < len; a++)
+		{
+			if (num[i] > num[a])
+			{
+				n++;
+			}
+		}
+	}
+	cout << n;
+	cout << endl;
+	return 0;
 }
 
 void printPairs(int num[], int len)
 {
-
+	cout << "此数组为：";
+	for (int i = 0; i < len; i++)
+	{
+		cout << num[i];
+		cout << " ";
+	}
+	cout << endl;
 }
 
 void test()
 {
 	int num1[] = { 1, 3 , 4 , 7,  2 };
 	int num2[] = { 9, 8, 7, 6, 5 };
+
 
 	// task  
 	// 1. print out all pairs 
@@ -22,5 +44,10 @@ void test()
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int num1[] = { 1, 3 , 4 , 7,  2 };
+	int num2[] = { 9, 8, 7, 6, 5 };
+	printPairs(num1, 5);
+	getRevseOrders(num1, 5);
+	printPairs(num2, 5);
+	getRevseOrders(num2, 5);
 }
