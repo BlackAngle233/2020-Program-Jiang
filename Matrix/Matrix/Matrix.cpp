79fa930395d88b9b00e14e21e8e600f2cf8b45c4
amidu <63142813+amidu6@users.noreply.github.com>
2020-04-11 19:11:31 +0800

    作业2

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -1,17 +1,38 @@
 ﻿#include <iostream>
 #include "Matrix.h"
+using namespace std;
 
-void test() 
+void test()
 {
 	int a[4][4];
-	inputMartix4x4(a);
-	printMartix4x4(a);
+	int b[4][4];
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cin >> b[i][j];
+		}
+	}
+	int c[4][4];
+	Add(a, b, c);
+	Print(c);
+	int d[4][4];
+	Minus(a, b, d);
+	Print(d);
+	int e[4][4];
+	Multiple(a, b, e);
+	Print(e);
 }
 
 int main()
 {
-    std::cout << "test:\n";
 	test();
-	int t = 0;
-	std::cin >> t;
+	return 0;
 }
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -1,40 +1,51 @@
 #include <iostream>
 #pragma once
+#ifndef MATRIX
+#define MATRIX
 using namespace std;
 
-void printMartix4x4(int m[4][4]) {
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			cout << m[i][j] << " ";
+void Print(int a[4][4])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << a[i][j] << "\t";
 		}
 		cout << endl;
 	}
 }
 
-void inputMartix4x4(int m[4][4]) {
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			cin >> m[i][j];
-			cout << " ";
+void Add(int a[4][4], int b[4][4], int c[4][4])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			c[i][j] = a[i][j] + b[i][j];
 		}
-		cout << endl;
 	}
 }
 
-void add(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-	
-	printMartix4x4(result);
+void Minus(int a[4][4], int b[4][4], int c[4][4])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			c[i][j] = a[i][j] - b[i][j];
+		}
+	}
 }
 
-void minus(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-
-	printMartix4x4(result);
+void Multiple(int a[4][4], int b[4][4], int c[4][4])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j] + a[i][2] * b[2][j] + a[i][3] * b[3][j];
+		}
+	}
 }
-
-void multiple(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-
-	printMartix4x4(result);
-}
+#endif //MAT4_H
