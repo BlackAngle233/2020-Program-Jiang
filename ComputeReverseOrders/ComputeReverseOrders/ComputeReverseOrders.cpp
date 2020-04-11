79ff2fb8c35390610676ec525b49563e8d7755d2
inistranger <59947399+inistranger@users.noreply.github.com>
2020-04-11 12:23:25 +0800

    update

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -1,12 +1,47 @@
-﻿#include <iostream>
+﻿// homework1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
 
-int getRevseOrders(int num[], int len)
+
+#include <iostream>
+
+using namespace std;
+
+
+int len = 5;
+int num1[] = { 1, 3 , 4 , 7,  2 };
+int num2[] = { 9, 8, 7, 6, 5 };
+
+void getRevseOrders(int num[], int len)
 {
+	int p = 0;
 
+	for (int i = len - 1; i > -1; i--)
+	{
+		len--;
+		for (int j = len - 1; j > -1; j--)
+		{
+			if (num[len] < num[j])
+				p++;
+		}
+	}
+	cout << p << endl;
 }
 
 void printPairs(int num[], int len)
 {
+	/*for (int i = 0; i < len-1; i++)
+	{
+		cout << num[i]<<num[i+1];
+	}
+	cout << endl;*/
+	for (int i = len - 1; i > -1; i--)
+	{
+		len--;
+		for (int j = len - 1; j > -1; j--)
+		{
+			cout << num[len] << num[j] << endl;
+		}
+	}
 
 }
 
@@ -22,5 +57,8 @@ void test()
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	printPairs(num1, len);
+	printPairs(num2, len);
+	getRevseOrders(num1, len);
+	getRevseOrders(num2, len);
 }
