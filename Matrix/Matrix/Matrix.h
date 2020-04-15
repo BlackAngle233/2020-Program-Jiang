1f81ea9516914588ef91b71bcb1a745e31e7ad71
KiraHau <46923871+KiraHau@users.noreply.github.com>
2020-04-15 11:26:22 +0800

    homework2

--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -23,18 +23,44 @@ void inputMartix4x4(int m[4][4]) {
 
 void add(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-	
+	int i = 0, j = 0;
+	for (i; i < 4; i++)
+	{
+		for (j=0; j < 4; j++)
+		{
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
-void minus(int m1[4][4], int m2[4][4]) {
+void minuss(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	int i = 0, j = 0;
+	for (i; i < 4; i++)
+	{
+		for (j=0; j < 4; j++)
+		{
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
 void multiple(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	int i = 0, j = 0;
+	for (i; i < 4; i++)
+	{
+		for (j=0; j < 4; j++)
+		{
+			result[i][j] = 0;
+			for (int n = 0; n < 4; n++)
+			{
+				result[i][j] = result[i][j]+ m1[i][n] + m2[n][j];
+			}
+			
+		}
+	}
 	printMartix4x4(result);
 }
