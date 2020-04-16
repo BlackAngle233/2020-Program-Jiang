e976bef9782c7662acc5d7ec185d8c803af73a9a
UbiquityBugsInsides <63142798+UbiquityBugsInsides@users.noreply.github.com>
2020-04-16 14:10:40 +0800

    Update DQ

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -4,14 +4,26 @@
 void test() 
 {
 	int a[4][4];
+	int b[4][4];
+	cout << "input first martix:" << endl;
 	inputMartix4x4(a);
 	printMartix4x4(a);
+	cout << "input second martix:" << endl;
+	inputMartix4x4(b);
+	printMartix4x4(b);
+	cout << "add martixs:" << endl;
+	addMartix(a, b);
+	cout << "minus martixs:" << endl;
+	minusMartix(a, b);
+	cout << "multiple martixs:" << endl;
+	multipleMartix(a, b);
 }
 
 int main()
 {
     std::cout << "test:\n";
 	test();
+	cout << "press any number to countinue..." << endl;
 	int t = 0;
 	std::cin >> t;
 }
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -5,7 +5,7 @@ using namespace std;
 void printMartix4x4(int m[4][4]) {
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
-			cout << m[i][j] << " ";
+			cout << m[i][j] << "	";
 		}
 		cout << endl;
 	}
@@ -15,26 +15,39 @@ void inputMartix4x4(int m[4][4]) {
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
 			cin >> m[i][j];
-			cout << " ";
 		}
 		cout << endl;
 	}
 }
 
-void add(int m1[4][4], int m2[4][4]) {
+void addMartix(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-	
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
-void minus(int m1[4][4], int m2[4][4]) {
+void minusMartix(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
-void multiple(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-
+void multipleMartix(int m1[4][4], int m2[4][4]) {
+	int result[4][4]{ 0 };
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+				result[i][j] = m1[i][k] * m2[k][j] + result[i][j];
+			}
+		}
+	}
 	printMartix4x4(result);
 }
