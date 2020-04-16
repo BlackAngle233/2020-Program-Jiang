e6f3723f78eb49e07d46155cb08bf0426b9936cf
Limbo414 <63141898+Limbo414@users.noreply.github.com>
2020-04-16 18:12:19 +0800

    HW2

---
+++ Matrix/Matrix/Matrix.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Matrix.h"
+
+void test() 
+{
+	int a[4][4];
+	inputMartix4x4(a);
+	printMartix4x4(a);
+}
+
+int main()
+{
+    std::cout << "test:\n";
+	test();
+	int t = 0;
+	std::cin >> t;
+}
