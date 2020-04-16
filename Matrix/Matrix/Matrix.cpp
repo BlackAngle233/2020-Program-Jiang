4d1d416f42aad9d61a8374f5e0f2e3df4af61068
inistranger <59947399+inistranger@users.noreply.github.com>
2020-04-16 23:03:04 +0800

    update

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -4,14 +4,21 @@
 void test() 
 {
 	int a[4][4];
+	int b[4][4];
 	inputMartix4x4(a);
-	printMartix4x4(a);
+	inputMartix4x4(b);
+	
+	add(a, b);
+	minus(a, b);
+	multiple(a, b);
+
 }
 
 int main()
 {
     std::cout << "test:\n";
 	test();
+	
 	int t = 0;
 	std::cin >> t;
 }
