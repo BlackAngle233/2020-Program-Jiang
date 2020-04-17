b735bae32027752c0fcae08dc06be98da50d9cbe
alan918 <1258164439@qq.com>
2020-04-17 15:12:32 +0800

    test2

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -4,14 +4,22 @@
 void test() 
 {
 	int a[4][4];
+	int b[4][4];
+	cout << "inputMartix4x4 A:" << endl;
 	inputMartix4x4(a);
-	printMartix4x4(a);
+	cout << "inputMartix4x4 B:" << endl;
+	inputMartix4x4(b);
+	int r1[4][4];
+	Add(a, b, r1);
+	int r2[4][4];
+	Minus(a, b, r2);
+	int r3[4][4];
+	Multiple(a, b, r3);
 }
 
 int main()
 {
-    std::cout << "test:\n";
+    std::cout << "test2:\n";
 	test();
-	int t = 0;
-	std::cin >> t;
+	return 0;
 }
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #pragma once
+#ifndef MATRIX
+#define MATRIX
 using namespace std;
 
 void printMartix4x4(int m[4][4]) {
@@ -12,8 +14,8 @@ void printMartix4x4(int m[4][4]) {
 }
 
 void inputMartix4x4(int m[4][4]) {
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			cin >> m[i][j];
 			cout << " ";
 		}
@@ -21,20 +23,39 @@ void inputMartix4x4(int m[4][4]) {
 	}
 }
 
-void add(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-	
-	printMartix4x4(result);
+void Add(int m1[4][4], int m2[4][4],int r1[4][4]) {
+	cout << "the rusult of A add B is:" << endl;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			r1[i][j] = m1[i][j] + m2[i][j];
+		}
+		cout << endl;
+	}
+	printMartix4x4(r1);
+	cout << endl;
 }
 
-void minus(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-
-	printMartix4x4(result);
+void Minus(int m1[4][4], int m2[4][4], int r2[4][4]) {
+	cout << "the rusult of A Minus B is:" << endl;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			r2[i][j] = m1[i][j] - m2[i][j];
+		}
+		cout << endl;
+	}
+	printMartix4x4(r2);
+	cout << endl;
 }
 
-void multiple(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-
-	printMartix4x4(result);
+void Multiple(int m1[4][4], int m2[4][4], int r3[4][4]) {
+	cout << "the rusult of A Multiple B is:" << endl;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			r3[i][j] = m1[i][j] * m2[i][j];
+		}
+		cout << endl;
+	}
+	printMartix4x4(r3);
+	cout << endl;
 }
+#endif //MAT4_H
