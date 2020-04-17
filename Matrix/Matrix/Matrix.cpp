d1e7e6e829c1c589bdbe1b21da2fae28209319ae
FFFpr <544205300+FFFpr@users.noreply.github.com>
2020-04-17 14:54:25 +0800

    update

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -10,8 +10,23 @@ void test()
 
 int main()
 {
-    std::cout << "test:\n";
-	test();
+    //std::cout << "test:\n";
+	//test();
 	int t = 0;
 	std::cin >> t;
+	int a[4][4];
+	int b[4][4];
+	while (t > 0) {
+		std::cout << "输入m1:" << endl;
+		inputMartix4x4(a);
+		std::cout << "输入m2:" << endl;
+		inputMartix4x4(b);std::cout << endl;
+		printMartix4x4(a);std::cout << endl;
+		printMartix4x4(b);std::cout << endl;
+		add(a, b);std::cout << endl;
+		//minus(a, b);
+		::minus(a, b);std::cout << endl;
+		multiple(a, b);
+		t--;
+	}
 }
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -23,18 +23,32 @@ void inputMartix4x4(int m[4][4]) {
 
 void add(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-	
+	for (int i = 0;i < 4;i++)
+		for (int j = 0;j < 4;j++) {
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
 	printMartix4x4(result);
 }
 
 void minus(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	for (int i = 0;i < 4;i++)
+		for (int j = 0;j < 4;j++) {
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
 	printMartix4x4(result);
 }
 
 void multiple(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	int sum;
+	for (int i = 0;i < 4;i++)
+		for (int j = 0;j < 4;j++) {
+			sum = 0;
+			for (int t = 0;t < 4;t++) {
+				sum += m1[i][t] * m2[t][j];
+			}
+			result[i][j] = sum;
+		}
 	printMartix4x4(result);
 }
