e15e7de31dfedd52068ae2b10ecec1af251cd298
Xuralon <63142911+Xuralon@users.noreply.github.com>
2020-04-17 17:40:10 +0800

    update

--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -15,7 +15,6 @@ void inputMartix4x4(int m[4][4]) {
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
 			cin >> m[i][j];
-			cout << " ";
 		}
 		cout << endl;
 	}
@@ -47,11 +46,24 @@ void substract(int m1[4][4], int m2[4][4]) {
 
 void multiple(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
+	for (int i = 0;i < 4;i++)
+	{
+		for (int j = 0;j < 4;j++)
+		{
+			result[i][j] = 0;
+		}
+	}
+	int tmp = 0;
 	for (int i = 0; i < 4; ++i)
 	{
 		for (int j = 0; j < 4; ++j)
 		{
-			result[i][j] = m1[i][j] * m2[j][i];
+			for (int k = 0;k < 4;k++)
+			{
+				tmp += m1[i][k] * m2[k][j];
+			}
+			result[i][j] = tmp;
+			tmp = 0;
 		}
 	}
 	printMartix4x4(result);
