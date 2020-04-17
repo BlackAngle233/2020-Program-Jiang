e3aa14962f1c5d7b229eccf3b5e32f8e22caed32
l-eunice <63146522+l-eunice@users.noreply.github.com>
2020-04-18 00:46:18 +0800

    finish2

---
+++ Matrix/Matrix/Matrix.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "Matrix.h"
+
+void test() 
+{
+	int a[4][4], b[4][4];
+	std::cout << "Input array1:" << endl;
+	inputMartix4x4(a);
+	std::cout << "Input array2:" << endl;
+	inputMartix4x4(b);
+	std::cout << "print array1:" << endl;
+	printMartix4x4(a);
+	std::cout << "print array2:" << endl;
+	printMartix4x4(b);
+	std::cout << "a+b=" << endl;
+	add(a, b);
+	std::cout << "a-b=" << endl;
+	substract(a, b);
+	std::cout << "a*b=" << endl;
+	multiple(a, b);
+}
+
+int main()
+{
+    std::cout << "test:\n";
+	test();
+}
---
+++ Matrix/Matrix/Matrix.h
@@ -0,0 +1,62 @@
+#include <iostream>
+#pragma once
+using namespace std;
+
+void printMartix4x4(int m[4][4]) {
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			cout << m[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void inputMartix4x4(int m[4][4]) {
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			cin >> m[i][j];
+			cout << " ";
+		}
+		cout << endl;
+	}
+}
+
+void add(int m1[4][4], int m2[4][4]) {
+	int result[4][4];
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j) 
+		{
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
+	printMartix4x4(result);
+}
+
+void substract(int m1[4][4], int m2[4][4]) {
+	int result[4][4];
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
+	}
+	printMartix4x4(result);
+}
+
+void multiple(int m1[4][4], int m2[4][4]) {
+	int result[4][4];
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			int sum = 0;
+			for (int k = 0; k < 4; k++) {
+				sum += m1[i][k] + m2[k][j];
+			}
+			result[i][j] = sum;
+		}
+	}
+	printMartix4x4(result);
+}
