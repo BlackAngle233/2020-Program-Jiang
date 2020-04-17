64ecc4ae487fcb7a67d91ac6e5807a2adee04de7
yjh214 <63141781+yjh214@users.noreply.github.com>
2020-04-17 13:04:48 +0800

    Update Matrix.cpp

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -1,16 +1,26 @@
-﻿#include <iostream>
+#include <iostream>
+#include"pch.h"
 #include "Matrix.h"
+using namespace std;
 
-void test() 
+void test()
 {
 	int a[4][4];
+	int b[4][4];
 	inputMartix4x4(a);
+	inputMartix4x4(b);
 	printMartix4x4(a);
+	cout<<endl;
+	printMartix4x4(b);
+
+	add(a, b);
+	minus1(a, b);
+	multiple(a, b);
 }
 
 int main()
 {
-    std::cout << "test:\n";
+	std::cout << "test:\n";
 	test();
 	int t = 0;
 	std::cin >> t;
