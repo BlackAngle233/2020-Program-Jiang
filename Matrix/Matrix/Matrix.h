99bb2ed5ac1748767982387e895da3200c50cb2d
yjh214 <63141781+yjh214@users.noreply.github.com>
2020-04-17 12:59:25 +0800

    Update Matrix.h

--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -22,19 +22,36 @@ void inputMartix4x4(int m[4][4]) {
 }
 
 void add(int m1[4][4], int m2[4][4]) {
+	cout << endl << "add:"<<endl;
 	int result[4][4];
-	
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
-void minus(int m1[4][4], int m2[4][4]) {
+void minus1(int m1[4][4], int m2[4][4]) {
+	cout << endl << "minus:" << endl;
 	int result[4][4];
-
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
 void multiple(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-
+	cout << endl << "multiple:"<<endl;
+	int result[4][4] = {0};
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			for (int k = 0; k < 4; ++k) {
+				result[i][j] += m1[i][k] * m2[k][j];
+			}
+		}
+	}
 	printMartix4x4(result);
 }
