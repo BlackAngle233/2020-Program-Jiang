cd4f83b61f4b95145e54731051a7731f21a63473
WGDINYA <1831726998@qq.com>
2020-04-17 19:52:54 +0800

    hw2

--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -2,7 +2,7 @@
 #pragma once
 using namespace std;
 
-void printMartix4x4(int m[4][4]) {
+void printMartix4x4 (int m[4][4]) {
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
 			cout << m[i][j] << " ";
@@ -11,7 +11,7 @@ void printMartix4x4(int m[4][4]) {
 	}
 }
 
-void inputMartix4x4(int m[4][4]) {
+void inputMartix4x4 (int m[4][4]) {
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
 			cin >> m[i][j];
@@ -30,7 +30,7 @@ void add(int m1[4][4], int m2[4][4]) {
 			result[i][j] = m1[i][j] + m2[i][j];
 		}
 	}
-	printMartix4x4(result);
+	printMartix4x4 (result);
 }
 
 void substract(int m1[4][4], int m2[4][4]) {
@@ -42,7 +42,7 @@ void substract(int m1[4][4], int m2[4][4]) {
 			result[i][j] = m1[i][j] - m2[i][j];
 		}
 	}
-	printMartix4x4(result);
+	printMartix4x4 (result);
 }
 
 void multiple(int m1[4][4], int m2[4][4]) {
@@ -54,5 +54,5 @@ void multiple(int m1[4][4], int m2[4][4]) {
 			result[i][j] = m1[i][j] * m2[j][i];
 		}
 	}
-	printMartix4x4(result);
+	printMartix4x4 (result);
 }
