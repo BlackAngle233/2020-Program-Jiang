23c935577223bbbfae6fb1a655dc6339c1bf9e94
MMMMMercury <63144115+MMMMMercury@users.noreply.github.com>
2020-04-18 00:34:52 +0800

    Update

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -1,17 +1,30 @@
 ﻿#include <iostream>
 #include "Matrix.h"
 
-void test() 
+void test()
 {
 	int a[4][4];
+	int b[4][4];
+	cout << "请输入四阶矩阵a：" << endl;
 	inputMartix4x4(a);
+	cout << "请输入四阶矩阵b：" << endl;
+	inputMartix4x4(b);
+	cout << "整理如下：" << endl;
 	printMartix4x4(a);
+	printMartix4x4(b);
+	cout << "加法结果如下：" << endl;
+	add(a, b);
+	cout << "减法结果如下：" << endl;
+	Minus(a, b);
+	cout << "乘法结果如下：" << endl;
+	multiple(a, b);
 }
 
 int main()
 {
-    std::cout << "test:\n";
+	std::cout << "test:\n";
 	test();
+
 	int t = 0;
 	std::cin >> t;
 }
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -23,18 +23,33 @@ void inputMartix4x4(int m[4][4]) {
 
 void add(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-	
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
-void minus(int m1[4][4], int m2[4][4]) {
+void Minus(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
 void multiple(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result[i][j] = 0;
+			for (int k = 0; k < 4; k++) {
+				result[i][j] += m1[i][k] * m2[k][j];
+			}
+		}
+	}
 	printMartix4x4(result);
 }
