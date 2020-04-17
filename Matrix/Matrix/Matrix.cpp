dd6cc85a1518f3f4aaad1f94a8fcfd7147258b3f
Reed-redd <63222197+Reed-redd@users.noreply.github.com>
2020-04-17 22:31:52 +0800

    Update

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -6,11 +6,11 @@ void test()
 	int a[4][4];
 	inputMartix4x4(a);
 	printMartix4x4(a);
-	cout << "与自身相加：" << endl;
+	cout << "相加：" << endl;
 	add(a, a);
-	cout << "与自身相减：" << endl;
+	cout << "相减：" << endl;
 	substract(a, a);
-	cout << "与自身相乘：" << endl;
+	cout << "相乘：" << endl;
 	multiple(a, a);
 }
 
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -3,17 +3,17 @@
 using namespace std;
 
 void printMartix4x4(int m[4][4]) {
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			cout << m[i][j] << " ";
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4;j++) {
+			cout << m[i][j] ;
 		}
 		cout << endl;
 	}
 }
 
 void inputMartix4x4(int m[4][4]) {
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
+	for (int i = 0; i < 4;i++) {
+		for (int j = 0; j < 4;j++) {
 			cin >> m[i][j];
 			cout << " ";
 		}
@@ -22,37 +22,37 @@ void inputMartix4x4(int m[4][4]) {
 }
 
 void add(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-	for (int i = 0; i < 4; ++i)
+	int m[4][4];
+	for (int i = 0; i < 4;i++)
 	{
-		for (int j = 0; j < 4; ++j) 
+		for (int j = 0; j < 4;j++) 
 		{
-			result[i][j] = m1[i][j] + m2[i][j];
+			m[i][j] = m1[i][j] + m2[i][j];
 		}
 	}
-	printMartix4x4(result);
+	printMartix4x4(m);
 }
 
 void substract(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-	for (int i = 0; i < 4; ++i)
+	int n[4][4];
+	for (int i = 0; i < 4;i++)
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4;j++)
 		{
-			result[i][j] = m1[i][j] - m2[i][j];
+			n[i][j] = m1[i][j] - m2[i][j];
 		}
 	}
-	printMartix4x4(result);
+	printMartix4x4(n);
 }
 
 void multiple(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-	for (int i = 0; i < 4; ++i)
+	int n[4][4];
+	for (int i = 0; i < 4;i++)
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4;j++)
 		{
-			result[i][j] = m1[i][j] * m2[j][i];
+			n[i][j] = m1[i][j] * m2[j][i];
 		}
 	}
-	printMartix4x4(result);
+	printMartix4x4(n);
 }
