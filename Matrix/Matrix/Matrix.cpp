f58d444cb360c421ef7b6c2a0081129bb057aeb7
giraffe1217 <51290300+giraffe1217@users.noreply.github.com>
2020-04-17 16:39:36 +0800

    update

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -3,15 +3,32 @@
 
 void test() 
 {
-	int a[4][4];
+	int a[4][4]; int b[4][4];
+
+    std::cout << "-------------------------------" << endl;
+	std::cout << "请输入矩阵A:" << endl;
 	inputMartix4x4(a);
-	printMartix4x4(a);
+
+    std::cout << "-------------------------------" << endl;
+	std::cout << "请输入矩阵B:" << endl;
+	inputMartix4x4(b);
+
+	std::cout << "-------------------------------" << endl;
+	std::cout << "A+B:" << endl;
+	Add(a, b);
+
+	std::cout << "-------------------------------" << endl;
+	std::cout << "A-B:" << endl;
+	Minus(a, b);
+
+	std::cout << "-------------------------------" << endl;
+	std::cout << "A*B:" << endl;
+	Multiple(a, b);
+
 }
 
 int main()
 {
     std::cout << "test:\n";
 	test();
-	int t = 0;
-	std::cin >> t;
 }
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -21,20 +21,32 @@ void inputMartix4x4(int m[4][4]) {
 	}
 }
 
-void add(int m1[4][4], int m2[4][4]) {
+void Add(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-	
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
-void minus(int m1[4][4], int m2[4][4]) {
+void Minus(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
-void multiple(int m1[4][4], int m2[4][4]) {
+void Multiple(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result[i][j] = m1[i][0] * m2[0][j] + m1[i][1] * m2[1][j] + m1[i][2] * m2[2][j] + m1[i][3] * m2[3][j];
+		}
+	}
 	printMartix4x4(result);
 }
