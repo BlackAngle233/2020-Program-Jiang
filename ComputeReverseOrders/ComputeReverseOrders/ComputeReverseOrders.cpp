3db4962cb3f8c6ac8d8464b59aea2e46641cbc45
aiyueer425 <58095047+aiyueer425@users.noreply.github.com>
2020-04-17 16:37:44 +0800

    Revert "Merge remote-tracking branch 'origin/冯沛然' into 李致远"
    This reverts commit 117f175115caa22f349462bab2c00713e5e4d19c, reversing
    changes made to 9892b9c02a91cab299e200e0b3a9e17edc40e6b9.

--- ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
+++ ComputeReverseOrders/ComputeReverseOrders/ComputeReverseOrders.cpp
@@ -1,27 +1,13 @@
 ﻿#include <iostream>
-using namespace std;
 
 int getRevseOrders(int num[], int len)
 {
-	int countSum = 0;
-	for (int i = 0;i < len - 1;i++) {
 
-		for (int j = i + 1;j < len;j++) {
-			if (num[j] < num[i])
-				countSum++;
-		}
-	}
-	return countSum;
 }
 
 void printPairs(int num[], int len)
 {
-	for (int i = 0;i < len - 1;i++) {
 
-		for (int j = i + 1;j < len;j++) {
-			cout << "(" << num[i] << "," << num[j] << ")" << endl;
-		}
-	}
 }
 
 void test()
@@ -36,11 +22,5 @@ void test()
 
 int main()
 {
-    //std::cout << "Hello World!\n";
-	int num1[] = { 1,3,4,7,2 };
-	int num2[] = { 9, 8, 7, 6, 5 };
-	printPairs(num1, 5);
-	cout << "num1的逆序列数为" << getRevseOrders(num1, 5) << endl;
-	printPairs(num2, 5);
-	cout << "num2的逆序列数为" << getRevseOrders(num2, 5) << endl;
+    std::cout << "Hello World!\n";
 }
