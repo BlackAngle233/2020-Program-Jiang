41fd7cfae01398941237b9551c5a248a6fdfecd6
BlackAngle233 <740448368@qq.com>
2020-04-18 13:13:26 +0800

    Revert "Revert "update1""
    This reverts commit 5ced68abde48e4882e37841763f76b4f6801ea03.

--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -23,18 +23,36 @@ void inputMartix4x4(int m[4][4]) {
 
 void add(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-	
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j) 
+		{
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
-void minus(int m1[4][4], int m2[4][4]) {
+void substract(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
+	}
 	printMartix4x4(result);
 }
 
 void multiple(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
-
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result[i][j] = m1[i][j] * m2[j][i];
+		}
+	}
 	printMartix4x4(result);
 }
