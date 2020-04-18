2ea395aed4930086ff6f99311783eb527e9c5aaf
Zhuoheng Shen <60502108+shen13522@users.noreply.github.com>
2020-04-18 23:06:42 +0800

    update

--- List/List/SingleList.cpp
+++ List/List/SingleList.cpp
@@ -1,2 +1,30 @@
 ﻿#include <iostream>
 #include "SingleList.h"
+
+//SingleList* create(int[], int len) {
+//
+//}
+
+void erase(SingleList* list) {
+
+}
+
+void insert(SingleList* list, int v) {
+
+}
+
+//SingleList::Node* find(int v) {
+//	
+//}
+
+void remove(SingleList::Node* node) {
+
+}
+
+void remove(SingleList* list, int v) {
+
+}
+
+void print(SingleList* list) {
+
+}
--- List/List/main.cpp
+++ List/List/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "SingleList.h"
 
+
+bool checkBit(int a,int n) {
+	return(a >> n & 1);
+}
 int main()
 {
 	std::cout << "Hello World!\n";
