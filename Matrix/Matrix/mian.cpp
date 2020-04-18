e461b108ae45a6b7548b50e64016ef390fdb3d69
Zhuoheng Shen <60502108+shen13522@users.noreply.github.com>
2020-04-18 14:44:09 +0800

    update

---
+++ Matrix/Matrix/mian.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include "mat4.h"
+using namespace std;
+
+void test() 
+{
+	int a[4][4] = { {1,2,3,4},{2,3,4,5},{3,4,5,6},{4,5,6,7} };
+	int b[4][4] = { {2,3,4,5},{3,4,5,6},{4,5,6,7},{5,6,7,8} };
+	int result[4][4];
+
+	cout << "\n和为：" << endl;
+	add4x4(a, b, result);
+	print(result);
+	cout << "\n差为：" << endl;
+	minus4x4(a, b, result);
+	print(result);
+	cout << "\n积为："<<endl;
+	multiple4x4(a, b, result);
+	print(result);
+
+
+
+}
+
+int main()
+{
+    std::cout << "test:\n";
+	test();
+}
