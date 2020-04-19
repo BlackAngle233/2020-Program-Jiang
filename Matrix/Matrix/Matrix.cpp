c5ae1e6a94e5eee93ffea29e2b9ede6d60787eaf
l-eunice <63146522+l-eunice@users.noreply.github.com>
2020-04-19 21:03:43 +0800

    second homework-2

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -1,27 +1,86 @@
-﻿#include <iostream>
+#include <iostream>
 #include "Matrix.h"
+using namespace std;
 
-void test() 
+void Matrix::printMartix4x4(int m[4][4]) {
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			cout << m[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void Matrix::inputMartix4x4(int m[4][4]) {
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			cin >> m[i][j];
+		}
+	}
+}
+
+void Matrix::add(int m1[4][4], int m2[4][4], int result[4][4]) {
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
+}
+
+void Matrix::substract(int m1[4][4], int m2[4][4], int result[4][4]) {
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result[i][j] = m1[i][j] - m2[i][j];
+		}
+	}
+}
+
+void Matrix::multiple(int m1[4][4], int m2[4][4], int result[4][4]) {
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			int sum = 0;
+			for (int k = 0; k < 4; k++) {
+				sum += m1[i][k] * m2[k][j]; 
+			}
+			result[i][j] = sum;
+		}
+	}
+}
+
+void test()
 {
-	int a[4][4], b[4][4];
-	std::cout << "Input array1:" << endl;
-	inputMartix4x4(a);
-	std::cout << "Input array2:" << endl;
-	inputMartix4x4(b);
-	std::cout << "print array1:" << endl;
-	printMartix4x4(a);
-	std::cout << "print array2:" << endl;
-	printMartix4x4(b);
-	std::cout << "a+b=" << endl;
-	add(a, b);
-	std::cout << "a-b=" << endl;
-	substract(a, b);
-	std::cout << "a*b=" << endl;
-	multiple(a, b);
+	int a[4][4], b[4][4],answer[4][4];
+	Matrix test;
+	cout << "Input array1:" << endl;
+	test.inputMartix4x4(a);
+	cout << "Input array2:" << endl;
+	test.inputMartix4x4(b);
+	cout << "print array1:" << endl;
+	test.printMartix4x4(a);
+	cout << "print array2:" << endl;
+	test.printMartix4x4(b);
+
+	cout << "a+b=" << endl;
+	test.add(a, b, answer);
+	test.printMartix4x4(answer);
+
+	cout << "a-b=" << endl;
+	test.substract(a, b, answer);
+	test.printMartix4x4(answer);
+
+	cout << "a*b=" << endl;
+	test.multiple(a, b, answer);
+	test.printMartix4x4(answer);
 }
 
 int main()
 {
-    std::cout << "test:\n";
+	cout << "test:\n";
 	test();
 }
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -1,62 +1,12 @@
-#include <iostream>
 #pragma once
+#include <iostream>
 using namespace std;
 
-void printMartix4x4(int m[4][4]) {
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			cout << m[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-
-void inputMartix4x4(int m[4][4]) {
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			cin >> m[i][j];
-			cout << " ";
-		}
-		cout << endl;
-	}
-}
-
-void add(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j) 
-		{
-			result[i][j] = m1[i][j] + m2[i][j];
-		}
-	}
-	printMartix4x4(result);
-}
-
-void substract(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			result[i][j] = m1[i][j] - m2[i][j];
-		}
-	}
-	printMartix4x4(result);
-}
-
-void multiple(int m1[4][4], int m2[4][4]) {
-	int result[4][4];
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			int sum = 0;
-			for (int k = 0; k < 4; k++) {
-				sum += m1[i][k] + m2[k][j];
-			}
-			result[i][j] = sum;
-		}
-	}
-	printMartix4x4(result);
-}
+class Matrix {
+public:
+	void printMartix4x4(int m[4][4]);
+	void inputMartix4x4(int m[4][4]);
+	void add(int m1[4][4], int m2[4][4], int result[4][4]);
+	void substract(int m1[4][4], int m2[4][4], int result[4][4]);
+	void multiple(int m1[4][4], int m2[4][4], int result[4][4]);
+};
