b33826790ec00705a5db49272a8fc6555bb44322
giraffe1217 <51290300+giraffe1217@users.noreply.github.com>
2020-04-19 17:16:01 +0800

    update

--- List/List/main.cpp
+++ List/List/main.cpp
@@ -3,5 +3,22 @@
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	int numList[] = { 1,2,3,4,5,4 };
+	SingleList* list = create(numList,6);
+	print(list);
+
+	//在最后插入一个值为3的结点
+	insert(list, 3);
+	print(list);
+
+	// 找到第一个值为4的结点并且删除它
+	remove(list, find(list, 4)) ;
+	print(list);
+
+	// 删除所有值为3的结点
+	remove(list, 3);
+	print(list);
+
+	// 删除单链表
+	erase(list);
 }
