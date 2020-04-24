1fd59d3c660ed9c2f3475177a298762bbc109310
Lulili33 <763333+Lulili33@users.noreply.github.com>
2020-04-24 23:35:08 +0800

    update

--- List/List/SingleList.h
+++ List/List/SingleList.h
@@ -10,5 +10,7 @@ struct SingleList
 	Node* head;
 };
 
+
+
 //定义一个单向链表
 //实现单向链表的 create erase insert find remove(两种) print 六个功能
--- List/List/main.cpp
+++ List/List/main.cpp
@@ -1,7 +1,140 @@
+
 #include <iostream>
-#include "SingleList.h"
+using namespace std;
+
+struct LinkList
+{
+	struct Node
+	{
+		Node *next = nullptr;
+	};
+	Node* head;
+	Node* next;
+	int value;
+};
+
+LinkList* creat(int a[], int len)
+{
+	LinkList *head = NULL;
+	LinkList *tail = NULL;
+	LinkList *add = NULL;
+	for (int i = 0; i < len; ++i)
+	{
+		LinkList *add = new LinkList(a[i]);
+		if (tail != NULL)
+		{
+			tail->next = add;
+			tail = add;
+		}
+		else
+		{
+			head = tail = add;
+		}
+	}
+	return head;
+}
+
+void erase(LinkList *&L, int len, int e)
+{
+	int j = 0;
+	LinkList *p = L, *q;
+	while (j < i - 1 && p != NULL)
+	{
+		j++;
+		p = p->next;
+	}
+	if (p == NULL)
+		false;
+	else
+	{
+		q = p->next;
+		if (q == NULL) false;
+		e = q->value;
+		p->next = q->next;
+		free(q);
+	}
+}
+
+void insert(LinkList* list, int num)
+{
+	int j = 0;
+	LinkList *p = list, *s;
+	while (j < num - 1 && p != NULL)
+	{
+		j++;
+		p = p->next;
+	}
+	if (p == NULL)
+		false;
+	else
+	{
+		s = (LinkList *)malloc(sizeof(LinkList));
+		s->value = num;
+		s->next = p->next;
+		p->next = s;
+	}
+}
+
+LinkList::Node* find(LinkList *L, int len, int e)
+{
+	int j = 0;
+	LinkList *p = L;
+	while (j < len && p != NULL)
+	{
+		j++;
+		p = p->next;
+	}
+	if (p == NULL)
+		return 0;
+	else
+	{
+		e = p->value;
+	}
+}
+
+void remove(LinkList *&L, int len, int e)
+{
+	int j = 0;
+	LinkList *p = L, *q;
+	while (j < len - 1 && p != NULL)
+	{
+		j++;
+		p = p->next;
+	}
+	if (p == NULL)
+		false;
+	else
+	{
+		q = p->next;
+		if (q == NULL) false;
+		e = q->value;
+		p->next = q->next;
+		free(q);
+	}
+}
+
+void remove(LinkList* list)
+{
+
+}
+
+void print(LinkList* list, int len)
+{
+	for (int i; i < len; i++)
+	{
+		cout << list->value << "  ";
+	}
+}
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	int a[] = { 1,2,3,4,5,7 };
+	LinkList *L;
+	L = creat(a, 6);
+	erase(L, 3, 4);
+	insert(L, 3);
+	find(L, 6, 2);
+	remove(L, 6, 5);
+	print(L, 6);
+
 }
