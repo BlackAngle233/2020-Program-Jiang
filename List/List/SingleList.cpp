956b2191e01277842332a84d619709c67a563f79
MMMMMercury <63144115+MMMMMercury@users.noreply.github.com>
2020-04-25 13:03:26 +0800

    update

--- List/List/SingleList.cpp
+++ List/List/SingleList.cpp
@@ -1,2 +1,20 @@
 ﻿#include <iostream>
 #include "SingleList.h"
+using namespace std;
+int main() {
+	int a[] = { 1,2,3,4,5 };
+	int i = 0;
+	int len = 1;
+	while (a[i] != NULL) {
+		i++;
+		len++;
+	}
+	SingleList* list;
+	list = create(a, len);
+	print(list);
+	SingleList::Node* node = find(list, 5);
+	insert(list, node, 6);
+	print(list);
+	erase(list);
+	print(list);
+}
