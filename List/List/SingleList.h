115dc9f3b6cef983c69d2683c0d7a02d5629144a
amidu <63142813+amidu6@users.noreply.github.com>
2020-05-15 23:03:45 +0800

    作业3改成class

--- List/List/SingleList.h
+++ List/List/SingleList.h
@@ -2,135 +2,267 @@
 #include<iostream>
 using namespace std;
 
-struct LinkList
+
+class Node
 {
-	struct Node
+public:
+	friend class LinkList;
+	Node(int v, Node* n)
 	{
-		Node* next = nullptr;
-		int value;
-	};
-	Node* head;
+		value = v;
+		next = n;
+	}
+	int value;
+	Node* next = nullptr;
 };
 
-LinkList* creat(int a[], int len)
+class LinkList
 {
-	LinkList* newlist = new LinkList;
-	LinkList::Node* p;
-	newlist->head = new LinkList::Node;
-	p = newlist->head;
-	for (int i = 0; i < len; i++)
+private:
+	Node* head;
+public:
+	LinkList()
 	{
-		p->next = new LinkList::Node;
-		p->next->value = a[i];
-		p = p->next;
+		head = nullptr;
 	}
-	p->next = nullptr;
-	return newlist;
-}
-
-void erase(LinkList* list)
-{
-	LinkList::Node* p;
-	while (list->head != NULL)
+	LinkList(int a[], int len)
 	{
-		p = list->head;
-		list->head = list->head->next;
-		delete p;
-	}
-}
-
-void insert(LinkList* list, int v)//这里实现了前插
-{
-	LinkList::Node* p;
-	LinkList::Node* q;
-	p = list->head;
-	q = list->head->next;
-	p->next = new LinkList::Node;
-	p->next->value = v;
-	p->next->next = q;
-}
-
-LinkList::Node* find(LinkList* list, int v)
-{
-	LinkList::Node* p;
-	p = list->head->next;
-	while (p != nullptr)
-	{
-		if (p->value == v)
+		Node* p;
+		head = new Node(-1, nullptr);
+		p = head;
+		for (int i = 0; i < len; i++)
 		{
-			return p;
-		}
-		else
-		{
+			p->next = new Node(a[i], nullptr);
 			p = p->next;
 		}
 	}
-	return p;
-}
-
-void remove(LinkList* list, LinkList::Node* node)//按节点删
-{
-	LinkList::Node* p;
-	p = list->head;
-	while (p->next != nullptr)
+	virtual ~LinkList()
 	{
-		if (p->next == node)
+		Node* p;
+		while (this->head != NULL)
 		{
-			LinkList::Node* q = p->next->next;
-			delete p->next;
-			p->next = q;
-		}
-		else
-		{
-			p = p->next;
+			p = this->head;
+			this->head = this->head->next;
+			delete p;
 		}
 	}
-}
-
-void remove(LinkList* list, int v)//按数字删
-{
-	LinkList::Node* p;
-	p = list->head;
-	while (p->next != nullptr)
+	void insert(int v)//这里实现了前插
 	{
-		if (p->next->value == v)
-		{
-			LinkList::Node* q = p->next->next;
-			delete p->next;
-			p->next = q;
-		}
-		else
-		{
-			p = p->next;
-		}
+		Node* p;
+		Node* q;
+		p = this->head;
+		q = this->head->next;
+		p->next = new Node(v, q);
 	}
-}
-
-void Print(LinkList* list)
-{
-	if (list->head != NULL)
+	Node* find(int v)
 	{
-		LinkList::Node* p;
-		p = list->head->next;
+		Node* p;
+		p = this->head->next;
 		while (p != nullptr)
 		{
-			if (p->next != nullptr)
+			if (p->value == v)
 			{
-				cout << p->value << "—>";
+				return p;
 			}
 			else
 			{
-				cout << p->value << endl;
+				p = p->next;
 			}
-			p = p->next;
+		}
+		return p;
+	}
+	void remove(Node* node)//按节点删
+	{
+		Node* p;
+		p = this->head;
+		while (p->next != nullptr)
+		{
+			if (p->next == node)
+			{
+				Node* q = p->next->next;
+				delete p->next;
+				p->next = q;
+			}
+			else
+			{
+				p = p->next;
+			}
 		}
 	}
-	else
+	void remove(int v)//按数字删
 	{
-		cout << "这玩意儿是空链表嗷" << endl;
+		Node* p;
+		p = this->head;
+		while (p->next != nullptr)
+		{
+			if (p->next->value == v)
+			{
+				Node* q = p->next->next;
+				delete p->next;
+				p->next = q;
+			}
+			else
+			{
+				p = p->next;
+			}
+		}
 	}
-	
+	void Print()
+	{
+		if (this->head != NULL)
+		{
+			Node* p;
+			p = this->head->next;
+			while (p != nullptr)
+			{
+				if (p->next != nullptr)
+				{
+					cout << p->value << "—>";
+				}
+				else
+				{
+					cout << p->value << endl;
+				}
+				p = p->next;
+			}
+		}
+		else
+		{
+			cout << "这玩意儿是空链表嗷" << endl;
+		}
+	}
+};
 
-}
+//
+//struct LinkList
+//{
+//	struct Node
+//	{
+//		Node* next = nullptr;
+//		int value;
+//	};
+//	Node* head;
+//};
+//
+//LinkList* creat(int a[], int len)
+//{
+//	LinkList* newlist = new LinkList;
+//	LinkList::Node* p;
+//	newlist->head = new LinkList::Node;
+//	p = newlist->head;
+//	for (int i = 0; i < len; i++)
+//	{
+//		p->next = new LinkList::Node;
+//		p->next->value = a[i];
+//		p = p->next;
+//	}
+//	p->next = nullptr;
+//	return newlist;
+//}
+//
+//void erase(LinkList* list)
+//{
+//	LinkList::Node* p;
+//	while (list->head != NULL)
+//	{
+//		p = list->head;
+//		list->head = list->head->next;
+//		delete p;
+//	}
+//}
+//
+//void insert(LinkList* list, int v)//这里实现了前插
+//{
+//	LinkList::Node* p;
+//	LinkList::Node* q;
+//	p = list->head;
+//	q = list->head->next;
+//	p->next = new LinkList::Node;
+//	p->next->value = v;
+//	p->next->next = q;
+//}
+//
+//LinkList::Node* find(LinkList* list, int v)
+//{
+//	LinkList::Node* p;
+//	p = list->head->next;
+//	while (p != nullptr)
+//	{
+//		if (p->value == v)
+//		{
+//			return p;
+//		}
+//		else
+//		{
+//			p = p->next;
+//		}
+//	}
+//	return p;
+//}
+//
+//void remove(LinkList* list, LinkList::Node* node)//按节点删
+//{
+//	LinkList::Node* p;
+//	p = list->head;
+//	while (p->next != nullptr)
+//	{
+//		if (p->next == node)
+//		{
+//			LinkList::Node* q = p->next->next;
+//			delete p->next;
+//			p->next = q;
+//		}
+//		else
+//		{
+//			p = p->next;
+//		}
+//	}
+//}
+//
+//void remove(LinkList* list, int v)//按数字删
+//{
+//	LinkList::Node* p;
+//	p = list->head;
+//	while (p->next != nullptr)
+//	{
+//		if (p->next->value == v)
+//		{
+//			LinkList::Node* q = p->next->next;
+//			delete p->next;
+//			p->next = q;
+//		}
+//		else
+//		{
+//			p = p->next;
+//		}
+//	}
+//}
+//
+//void Print(LinkList* list)
+//{
+//	if (list->head != NULL)
+//	{
+//		LinkList::Node* p;
+//		p = list->head->next;
+//		while (p != nullptr)
+//		{
+//			if (p->next != nullptr)
+//			{
+//				cout << p->value << "—>";
+//			}
+//			else
+//			{
+//				cout << p->value << endl;
+//			}
+//			p = p->next;
+//		}
+//	}
+//	else
+//	{
+//		cout << "这玩意儿是空链表嗷" << endl;
+//	}
+//	
+//
+//}
 //定义一个单向链表
 //实现单向链表的 create erase insert find remove(两种) print 六个功能
--- List/List/main.cpp
+++ List/List/main.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include <iostream>
 #include "SingleList.h"
 using namespace std;
@@ -6,19 +5,30 @@ using namespace std;
 void test()
 {
 	int a[5] = { 1,2,3,4,5 };
-	LinkList* linklist;
-	linklist = creat(a, 5);
-	Print(linklist);
-	insert(linklist, 5);
-	Print(linklist);
-	LinkList::Node* p = find(linklist, 4);
+	LinkList* linklist = new LinkList(a, 5);
+	linklist->Print();
+	linklist->insert(5);
+	linklist->Print();
+	Node* p = linklist->find(4);
 	cout << p->value << "->" << p->next->value << endl;
-	remove(linklist, p);
-	Print(linklist);
-	remove(linklist, 5);
-	Print(linklist);
-	erase(linklist);
-	Print(linklist);
+	linklist->remove(p);
+	linklist->Print();
+	linklist->remove(5);
+	linklist->Print();
+
+	//int a[5] = { 1,2,3,4,5 };
+	//LinkList* linklist = new LinkList(a, 5);
+	//Print(linklist);
+	//insert(linklist, 5);
+	//Print(linklist);
+	//LinkList::Node* p = find(linklist, 4);
+	//cout << p->value << "->" << p->next->value << endl;
+	//remove(linklist, p);
+	//Print(linklist);
+	//remove(linklist, 5);
+	//Print(linklist);
+	//erase(linklist);
+	//Print(linklist);
 }
 
 int main()
