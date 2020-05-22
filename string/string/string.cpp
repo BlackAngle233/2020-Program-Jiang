515d7bf322dabfd8bf4d3eb8a4edd7096dd61d99
JiCheng1 <1093501020@qq.com>
2020-05-22 22:53:17 +0800

    update

--- string/string/string.cpp
+++ string/string/string.cpp
@@ -7,43 +7,42 @@ public:
 	explicit String(const char* str = "");
 	explicit String(const String& other);
 	String& operator=(const String& assign);
-	~String() { delete[] m_data; }
-	operator char* () { return m_data; }
+	~String() { delete[] data; }
+	operator char* () { return data; }
 private:
-	char* m_data;
+	char* data;
 };
 
 String::String(const char* str) {
 	if (str == NULL) {
-		m_data = new char[1];
-		*m_data = '/0';
+		data = new char[1];
+		*data = '/0';
 	}
 	else {
-		m_data = new char[strlen(str) + 1];
+		data = new char[strlen(str) + 1];
 	}
 }
 
 String::String(const String& other){
-	m_data = new char[strlen(other.m_data) + 1];
+	data = new char[strlen(other.data) + 1];
 
 }
 
 String& String::operator=(const String& assign){
 	if (this != &assign){
-		delete[] m_data;
-		char* temp = new char[strlen(assign.m_data) + 1];
-		m_data = temp;
+		delete[] data;
+		char* temp = new char[strlen(assign.data) + 1];
+		data = temp;
 	}
 	return *this;
 }
 
-int main(){
-	String s("hahahaha");
-	String s1(s);
-	String* s2 = new String("xixixixi");
-	String s3(*s2);
-	cout << s << endl;
-	cout << s1 << endl;
-	cout << *s2 << endl;
-	cout << s3 << endl;
+bool empty() {
+	string str;
+	if (str == "") {
+		return 0;
+	}
+	else {
+		return 1;
+	}
 }
