5c4cabc6cf0d4941c703d1461b8ee2546a8d8111
l-eunice <63146522+l-eunice@users.noreply.github.com>
2020-05-23 12:24:26 +0800

    forth homework-1

---
+++ MyString/MyString/MyString.cpp
@@ -0,0 +1,102 @@
+#include"MyString.h"
+#include<iostream>
+#include<memory>
+using namespace std;
+
+MyString::MyString() {
+	Buf* b = new Buf{0,nullptr};
+	buff_.reset(b);
+}
+
+MyString::MyString(char c) {
+	Buf* b = new Buf{ 1,&c };
+	buff_.reset(b);
+}
+
+MyString::MyString(const char* p) {
+	int i = 0;
+	char* pe=new char();
+	while (p[i]!=NULL) {
+		pe[i] = p[i];
+		i++;
+	}
+	Buf* b = new Buf{ (size_t)i, pe };
+	buff_.reset(b);
+}
+
+MyString::MyString(const MyString& other) {
+	char* pe = new char();
+	int i = 0;
+	while (other.buff_->p+i) {
+		*(pe + i )= *(other.buff_->p + i);
+		i++;
+	}
+	Buf* b = new Buf{ (size_t)i, pe };
+	buff_.reset(b);
+}
+
+MyString& MyString::operator=(const MyString& other) {
+	char* pe = new char();
+	int i = 0;
+	while (other.buff_->p + i) {
+		*(pe + i) = *(other.buff_->p + i);
+		i++;
+	}
+	buff_->length = i;
+	buff_->p = pe;
+	return *this;
+}
+
+MyString::~MyString() {
+
+}
+
+MyString::MyString(MyString&& other)
+	:buff_{other.buff_}
+{
+	other.buff_->length = 0;
+	other.buff_->p = nullptr;
+}
+
+MyString& MyString::operator=(MyString&& other) {
+	std::shared_ptr<Buf > tmp;
+	tmp = std::move(buff_);
+	buff_ = std::move(other.buff_);
+	other.buff_ = std::move(tmp);
+	return *this;
+}
+
+MyString& MyString::operator +(const MyString& other) {
+	buff_->length = buff_->length + other.buff_->length;
+	int i = 0;
+	while (other.buff_->p + i) {
+		*(buff_->p + buff_->length+ i) = *(other.buff_->p + i);
+		i++;
+	}
+	return *this;
+}
+
+bool MyString::empty() const {
+	if (!buff_)
+		return true;
+	else
+		return buff_->length > 0 ? false : true;
+}
+
+size_t MyString::length()const {
+	return buff_ ? buff_->length : 0;
+}
+
+void MyString::print() {
+	if (empty()) {
+		cout << "MyString对象为空" << endl;
+	}
+	else {
+		int i = 0;
+		cout << "长度：" << buff_->length<<endl;
+		while (buff_->p+i) {
+			cout << *(buff_->p + i) << endl;
+			i++;
+		}
+	}
+}
---
+++ MyString/MyString/MyString.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<memory>
+class MyString {
+public:
+	using iterator = char*;
+	iterator begin() { 
+		return buff_ ? buff_->p : nullptr; 
+	}
+	iterator end() {
+		return buff_ ? buff_->p + buff_->length : nullptr;
+	}
+	MyString();
+	MyString(char c);
+	MyString(const char* p);
+	~MyString();
+	MyString(const MyString& other);//拷贝构造
+	MyString& operator=(const MyString& other);//拷贝赋值
+
+	MyString(MyString&& other);//移动构造
+	MyString& operator=(MyString&& other);//移动赋值
+	bool empty() const;
+	size_t length()const;
+	MyString& operator+(const MyString& other);
+
+	void print();//打印
+
+private:
+	struct Buf {
+		size_t length;
+		char* p;
+	};
+	std::shared_ptr<Buf >buff_;
+};
