b35a7294dbff209b653838887c08fc9d3041dc92
季枝 <869118657@qq.com>
2020-06-04 20:01:05 +0800

    123

--- 复数/复数/源.cpp
+++ 复数/复数/源.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-class complex {	//....private:	double re;	double im;public:	constexpr double real() const { return re; }	constexpr double imag() const { return im; }	complex(double r = 0, double i = 0)		: re{ r }, im{ i }	{ }	complex& operator+= (double a)	{		re += a;		return *this;	}		bool operator==(complex b)	{		if (re == b.real() && im == b.imag())			return true;		else			return false;	}	// ....	complex operator+(double b)	{		return { re + b, im };	}	complex operator+(complex b)	{		return { re + b.real() , im + b.imag };	}	complex operator-(double b)	{		return { re - b, im };	}	complex operator-(complex b)	{		return { re - b.real() , im - b.imag };	}	complex operator*(complex& c);
+class complex {	//....	//我加了一行注释private:	double re;	double im;public:	constexpr double real() const { return re; }	constexpr double imag() const { return im; }	complex(double r = 0, double i = 0)		: re{ r }, im{ i }	{ }	complex& operator+= (double a)	{		re += a;		return *this;	}		bool operator==(complex b)	{		if (re == b.real() && im == b.imag())			return true;		else			return false;	}	// ....	complex operator+(double b)	{		return { re + b, im };	}	complex operator+(complex b)	{		return { re + b.real() , im + b.imag };	}	complex operator-(double b)	{		return { re - b, im };	}	complex operator-(complex b)	{		return { re - b.real() , im - b.imag };	}	complex operator*(complex& c);
 	complex operator/(complex& c);
 };
 
